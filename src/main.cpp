79d92dc0d3ab8b403dc9748b86458dbd7de3a3c3
Erik Junsved <dev@erix.dev>
2024-10-04 11:44:00 +0200

    add some sample voltage measurement code

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,35 @@ int main() {
 	Serial.begin(115200);
 	Serial.print("Initializing...");
 	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(3, OUTPUT);
 	Serial.println(" Done");
 
 	// Main program loop
 	// ReSharper disable once CppDFAEndlessLoop
-	for (;;) {
-		digitalWrite(LED_BUILTIN, LOW);
-		delay(128);
-		digitalWrite(LED_BUILTIN, HIGH);
-		delay(64);
+	//for (;;) {
+	//	const int val = analogRead(A0);
+	//	Serial.println(val/1023.0*5.0);
+	//}
+
+	//analogWrite(11, 128);
+	//for (int i = 0;; ++i) {
+	//	analogWrite(11, i%255);
+	//	delay(10);
+	//}
+
+	// Main program loop
+	// ReSharper disable once CppDFAEndlessLoop
+	for (long i = 0;; ++i) {
+		int val = analogRead(A0);
+		double ratio = val/1023.0;
+
+		int analogVal = (int)(ratio*255);
+
+		analogWrite(3, analogVal);
+
+		if (i % 1000 == 0) {
+			Serial.println(ratio);
+		}
+		delay(1);
 	}
 }
